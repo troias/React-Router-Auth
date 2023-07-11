e4428a5b5609393e9eae692ef6fe7feca00859a4
troias <hunter007nz@gmail.com>
2023-07-11 22:49:31 +1200

    added auth and a gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,36 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage, {
+  action as AuthAction,
+} from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "/auth",
+        element: <AuthenticationPage />,
+        action: AuthAction,
+      },
+      {
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
@@ -30,9 +38,10 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
+
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -41,21 +50,21 @@ const router = createBrowserRouter([
                 action: deleteEventAction,
               },
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
               },
             ],
           },
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
           },
         ],
       },
       {
-        path: 'newsletter',
+        path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
       },
